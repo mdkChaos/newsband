d10c881148233c528b6b40f66761d6cde0f77b65
mdkChaos <mdkchaos1@gmail.com>
2016-06-21 22:39:19 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-angular.module("MyModule", [])
-.controller("MyCtrl", function ($scope, $http) {
-    //$http сервис позволяет работать с ajax
+var ang = angular.module("studyModule", []);
+ang.controller("studyCtrl", function ($scope, $http) {
+	$scope.sortparam = "-date";
     $scope.getNews = function () {
         $http.get("data.json").success(function (responseData) {
-            //вызвав метод get сервиса выполняем GET запрос
-            $scope.fruits = dataresponseData;
-            //полученные данные записываем в свойство products на $scope, далее AngularJS самостоятельно обновит все привязки
+            $scope.news = responseData;
         })
-    }
+    };
 });
